e1ca13feb0dd097584f20337fbadd7f8df784e04
Joy Jain <joyjain184@gmail.com>
2026-06-25 22:41:42 +0530

    added toml file

--- app/api/main.py
+++ app/api/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI, Query
 from app.rag.engine import RAGEngine
+from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 rag_engine=RAGEngine()
 
